413e4c4a2a50da41ea8368c4ba5785ba06bc15b8
Aled <aledjones@nhs.net>
2017-02-15 01:26:28 +0000

    first addition of app

---
+++ src/code.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# The following line causes bash to exit at any point if there is any error
+# and to output each line as it is executed -- useful for debugging
+set -e -x -o pipefail
+
+dx-download-all-inputs --parallel
+
+mv ~/in/gatk_jar_file/* ~/GenomeAnalysisTK.jar
+mv ~/in/inhouse_annotations/* ~/inhouse.vcf.gz
+mv ~/in/test_vcf/* ~/test.vcf
+mv ~/in/reference_genome/* ~/genome.fa
+
+# Show all the java versions installed on this worker
+# Show the java version the worker is using
+echo $(java -version)
+
+#
+# #mark-section "downloading reference genome"
+# genome=b37
+# subgenome=hs37d5
+# genome=hg19
+# subgenome=hg19
+
+# APPDATA=project-B6JG85Z2J35vb6Z7pQ9Q02j8
+# dx cat "$APPDATA:/misc/gatk_resource_archives/gatk.resources.${genome}.tar" | tar xf -
+# dx cat "$APPDATA:/misc/gatk_resource_archives/${subgenome}.fasta-index.tar.gz" | tar zxf -
+
+#ls
+
+# Use java7 as the default java version. 
+# If java7 doesn't work with the GATK version (3.6 and above) then switch to java8 and try again.
+#
+update-alternatives --set java /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java
+java -jar GenomeAnalysisTK.jar -version || (update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java && java -jar GenomeAnalysisTK.jar -version)
+
+#
+# Calculate 80% of memory size, for java
+#
+head -n1 /proc/meminfo | awk '{print int($2*0.8/1024)}' >.mem_in_mb.txt
+java="java -Xmx$(<.mem_in_mb.txt)m"
+
+samtools faidx genome.fa
+java -jar picard.jar CreateSequenceDictionary R= genome.fa O= genome.dict
+
+tabix -p vcf ~/inhouse.vcf.gz
+
+$java -jar GenomeAnalysisTK.jar -T VariantAnnotator -R genome.fa -o output.inhouse.vcf.gz --resource:Inhouse ~/inhouse.vcf.gz --resourceAlleleConcordance -E Inhouse.PreviousClassification -V ~/inhouse.vcf.gz
+tabix -p vcf output.inhouse.vcf.gz
+
+#mark-section "uploading results"
+mkdir -p ~/out/vcf/output/ 
+mv output.inhouse.vcf.gz ~/out/vcf/output/"$test_vcf_prefix".inhouse.vcf.gz
+mv output.inhouse.vcf.gz.tbi ~/out/vcf/output/"$test_vcf_prefix".inhouse.vcf.gz.tbi
+
+#
+# Upload results
+#
+dx-upload-all-outputs --parallel
+#mark-success
